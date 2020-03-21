55326f6735f87f429049635dde77ef8b08d4ae43
Charlie Jiang <cqjjjzr@126.com>
2020-03-21 09:47:42 +0800

    :sparkles: (bili.live): Add gRPC room id updating module.

--- src/shared/config.cpp
+++ src/shared/config.cpp
@@ -11,18 +11,27 @@ std::shared_ptr<boost::program_options::variables_map> parse_options(int argc, c
     using namespace boost::program_options;
     auto desc = create_description();
     std::shared_ptr<variables_map> result = std::make_shared<variables_map>();
-    store(parse_command_line(argc, argv, desc), *result);
+    try
+    {
+        store(parse_command_line(argc, argv, desc), *result);
+    }
+    catch (boost::program_options::unknown_option& ex)
+    {
+        std::cerr << ex.what() << std::endl;
+        std::cerr << desc << std::endl;
+        std::exit(-1);
+    }
 
     if (result->count("help"))
     {
         std::cerr << desc << std::endl;
-        return std::shared_ptr<variables_map>();
+        std::exit(-1);
     }
 
     if (result->count("version"))
     {
         std::cerr << VERSION << std::endl;
-        return std::shared_ptr<variables_map>();
+        std::exit(-1);
     }
     return result;
 }
--- src/supervisor/config.cpp
+++ src/supervisor/config.cpp
@@ -1,14 +1,32 @@
 #include "config.h"
-#include "version.h"
-
-#include <iostream>
 
 namespace vNerve::bilibili::config
 {
+const std::string DEFAULT_GRPC_SERVER = "localhost";
+const int DEFAULT_GRPC_SERVER_PORT = 6161;
+const int DEFAULT_GRPC_UPDATE_INTERVAL_MINUTES = 60;
+
 boost::program_options::options_description create_description()
 {
+    // clang-format off
     using namespace boost::program_options;
+    auto descGeneric = options_description("Generic options");
+    descGeneric.add_options()
+        ("help", "Show help.")
+        ("version", "Show version")
+    ;
+
+    auto descGrpc = options_description("vNerve bilibili info gRPC server options");
+    descGrpc.add_options()
+        ("grpc-server,s", value<std::string>()->default_value(DEFAULT_GRPC_SERVER), "vNerve gRPC server host.")
+        ("grpc-server-port,p", value<int>()->default_value(DEFAULT_GRPC_SERVER_PORT), "vNerve gRPC server port.")
+        ("grpc-update-interval,u", value<int>()->default_value(DEFAULT_GRPC_UPDATE_INTERVAL_MINUTES), "vNerve VTuber list updating interval(min).")
+    ;
+
     auto desc = options_description("vNerve Bilibili Livestream chat crawling supervisor");
+    desc.add(descGeneric);
+    desc.add(descGrpc);
     return desc;
+    // clang-format on
 }
 }
