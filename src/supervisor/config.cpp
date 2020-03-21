439547be89892fef88d6f94173235e7dbaba4567
Charlie Jiang <cqjjjzr@126.com>
2020-03-21 16:40:26 +0800

    :wrench: (bili.live): Add supervisor configuations.

--- src/supervisor/config.cpp
+++ src/supervisor/config.cpp
@@ -6,6 +6,9 @@ const std::string DEFAULT_GRPC_SERVER = "localhost";
 const int DEFAULT_GRPC_SERVER_PORT = 6161;
 const int DEFAULT_GRPC_UPDATE_INTERVAL_MINUTES = 60;
 
+const std::string DEFAULT_BIND_HOST = "*";
+const int DEFAULT_BIND_PORT = 2525;
+
 boost::program_options::options_description create_description()
 {
     // clang-format off
@@ -21,6 +24,12 @@ boost::program_options::options_description create_description()
         ("grpc-server,s", value<std::string>()->default_value(DEFAULT_GRPC_SERVER), "vNerve gRPC server host.")
         ("grpc-server-port,p", value<int>()->default_value(DEFAULT_GRPC_SERVER_PORT), "vNerve gRPC server port.")
         ("grpc-update-interval,u", value<int>()->default_value(DEFAULT_GRPC_UPDATE_INTERVAL_MINUTES), "vNerve VTuber list updating interval(min).")
+    ;
+
+    auto descWorker = options_description("Worker settings");
+    descWorker.add_options()
+        ("port, P", value<int>()->default_value(DEFAULT_BIND_PORT), "Worker MQ binding port.")
+        ("host, H", value<std::string>()->default_value(DEFAULT_BIND_HOST), "Worker MQ binding host. Default to '*'")
     ;
 
     auto desc = options_description("vNerve Bilibili Livestream chat crawling supervisor");
