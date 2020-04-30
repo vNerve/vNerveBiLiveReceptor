0d99cf4fbbfe77aa902c4e0164bcfa30b7095781
H4M5TER <zhuxx2003@gmail.com>
2020-05-01 01:15:22 +0800

    :recycle: (serialize): Refactor type check and error handling.

--- src/worker/bili_json.cpp
+++ src/worker/bili_json.cpp
@@ -10,6 +10,7 @@
 #include <rapidjson/document.h>
 #include <rapidjson/encodings.h>
 #include <google/protobuf/arena.h>
+#include <spdlog/spdlog.h>
 
 #include <functional>
 #include <string>
@@ -31,156 +32,150 @@ namespace vNerve::bilibili
 const size_t JSON_BUFFER_SIZE = 128 * 1024;
 const size_t PARSE_BUFFER_SIZE = 32 * 1024;
 
-// #define CMD(name) robin_hood::pair<const string, function<void(const Document&, RoomMessage*, Arena*)>>(name, [](const Document& document, RoomMessage* message, Arena* arena)
-// #define CMD_END() )
-// // clang-format off
-// const robin_hood::unordered_map<string, function<void(const Document&, RoomMessage*, Arena*)>> command({
-//     CMD("DANMU_MSG")
-//     {
-//         auto const& info = document["info"].GetArray().Begin();
-//         auto const& basic_info = info->GetArray().Begin();
-//         auto const& user_info = (info + 2)->GetArray().Begin();
-//     }
-//     CMD_END(),
-// });
-// // clang-format on
-// #undef CMD
-// #undef CMD_END
-
 #define CMD(name)                                                         \
-    void cmd_##name(const Document&, RoomMessage*, Arena*);               \
+    bool cmd_##name(const Document&, RoomMessage*, Arena*);               \
     bool cmd_##name##_inited = command.emplace(#name, cmd_##name).second; \
-    void cmd_##name(const Document& document, RoomMessage* message, Arena* arena)
+    bool cmd_##name(const Document& document, RoomMessage* message, Arena* arena)
 
 // TODO: 更好的命名
-#define ASSERT_TRACE(expression) \
-    if (!(expression))           \
-    {                            \
-        /* TODO: SPDLOG_TRACE */ \
-    }                            \
-    else
-
-// TODO: 更好的命名
-#define ASSERT_TRACE_THROW(expression) \
-    if (!(expression))                 \
-    {                                  \
-        /* TODO: SPDLOG_TRACE */       \
-        throw #expression;             \
+#define ASSERT_TRACE(expr)                                                   \
+    if (!(expr))                                                             \
+    {                                                                        \
+        SPDLOG_TRACE("[bili_json] bilibili json type check failed: " #expr); \
+        return false;                                                        \
     }
 
 // 这个库似乎没有提供指定初始化容量的构造函数
-robin_hood::unordered_map<string, function<void(const Document&, RoomMessage*, Arena*)>> command;
+robin_hood::unordered_map<string, function<bool(const Document&, RoomMessage*, Arena*)>> command;
 
 CMD(DANMU_MSG)
 {
-    // 以下变量类型均为 const rapidjson::GenericArray::Iterator&
-    // 如何能够使用++iter并不产生内存开销？
-    ASSERT_TRACE_THROW(document["info"].IsArray())
-    auto const& info_iter = document["info"].Begin();
-    ASSERT_TRACE_THROW((info_iter)->IsArray())
-    auto const& basic_info_iter = (info_iter)->Begin();
-    ASSERT_TRACE_THROW((info_iter + 2)->IsArray())
-    auto const& user_info_iter = (info_iter + 2)->Begin();
-    // 需要测试message使用完毕清空时embedded message是否会清空
-    // 以下变量类型均为 T*
-    auto user_message = Arena::CreateMessage<live::UserMessage>(arena);
-    auto user_info = Arena::CreateMessage<live::UserInfo>(arena);
-    auto danmaku = Arena::CreateMessage<live::DanmakuMessage>(arena);
+    // TODO: 非结构错误的错误处理
+    // TODO: 需要测试message使用完毕清空时embedded message是否会清空
 
     // 尽管所有UserMessage都需要设置UserInfo
     // 但是不同cmd的数据格式也有所不同
     // 因此设置UserInfo和设置RMQ的topic都只能强耦合在每个处理函数里
 
+    ASSERT_TRACE(document.HasMember("info"))
+    ASSERT_TRACE(document["info"].IsArray())
+    // 数组数量不对是结构性错误
+    // 但b站很有可能在不改变先前字段的情况下添加字段
+    // 或许应改成document["info"].MemberCount() >= 15
+    // 这几个变量都是什么类型
+    ASSERT_TRACE(document["info"].MemberCount() == 15);
+    auto const& info = document["info"];  // 或 document["info"].GetArray()?
+    ASSERT_TRACE(info[0].IsArray())
+    ASSERT_TRACE(info[0].MemberCount() == 11)
+    auto const& basic_info = info[0];
+    ASSERT_TRACE(info[2].IsArray())
+    ASSERT_TRACE(info[2].MemberCount() == 8)
+    auto const& user_info = info[2];
+    // 以下变量类型均为 T*
+    auto embeded_user_message = Arena::CreateMessage<live::UserMessage>(arena);
+    auto embeded_user_info = Arena::CreateMessage<live::UserInfo>(arena);
+    auto embeded_danmaku = Arena::CreateMessage<live::DanmakuMessage>(arena);
+
     // user_info
-    ASSERT_TRACE((user_info_iter)->IsUint64())
+    ASSERT_TRACE(user_info[0].IsUint64())
     // uid
-    user_info->set_uid(user_info_iter->GetUint64());
-    ASSERT_TRACE((user_info_iter + 1)->IsString())
+    embeded_user_info->set_uid(user_info[0].GetUint64());
+    ASSERT_TRACE(user_info[1].IsString())
     // uname
-    user_info->set_name((user_info_iter + 1)->GetString(), (user_info_iter + 1)->GetStringLength());
-    ASSERT_TRACE((user_info_iter + 2)->IsBool())
+    embeded_user_info->set_name(user_info[1].GetString(), user_info[1].GetStringLength());
+    ASSERT_TRACE(user_info[2].IsBool())
     // admin
-    user_info->set_admin((user_info_iter + 2)->GetBool());
-    ASSERT_TRACE((user_info_iter + 3)->IsBool() && (user_info_iter + 4)->IsBool())
+    embeded_user_info->set_admin(user_info[2].GetBool());
+    ASSERT_TRACE(user_info[3].IsBool() && user_info[4].IsBool())
     // 这两个字段分别是月费/年费会员
     // 都为假时无会员 都为真时报错
-    if ((user_info_iter + 3)->GetBool() == (user_info_iter + 4)->GetBool())
+    if (user_info[3].GetBool() == user_info[4].GetBool())
     {
-        if ((user_info_iter + 3)->GetBool())
-            ;  // TODO: 错误处理
-        else
-            user_info->set_vip_level(live::LiveVipLevel::NO_VIP);
+        if (user_info[3].GetBool())
+            ;  // 均为真 报错 TODO: 错误处理
+        else   // 均为假 无直播会员
+            embeded_user_info->set_vip_level(live::LiveVipLevel::NO_VIP);
     }
     else
     {
-        if ((user_info_iter + 3)->GetBool())
-            user_info->set_vip_level(live::LiveVipLevel::MONTHLY);
-        else
-            user_info->set_vip_level(live::LiveVipLevel::YEARLY);
+        if (user_info[3].GetBool())
+            // 月费会员为真 年费会员为假 月费
+            embeded_user_info->set_vip_level(live::LiveVipLevel::MONTHLY);
+        else  // 月费会员为假 年费会员为真 年费
+            embeded_user_info->set_vip_level(live::LiveVipLevel::YEARLY);
     }
-    ASSERT_TRACE((user_info_iter + 5)->IsNumber())
-    if (10000 == (user_info_iter + 5)->GetInt())
+    ASSERT_TRACE(user_info[5].IsNumber())
+    if (10000 == user_info[5].GetInt())
     {
-        user_info->set_regular_user(true);
+        embeded_user_info->set_regular_user(true);
     }
-    else if (5000 == (user_info_iter + 5)->GetInt())
+    else if (5000 == user_info[5].GetInt())
     {
-        user_info->set_regular_user(false);
+        embeded_user_info->set_regular_user(false);
     }
     else
     {
         // TODO: 错误处理
     }
-    ASSERT_TRACE((user_info_iter + 6)->IsBool())
+    ASSERT_TRACE(user_info[6].IsBool())
     // phone_verified
-    user_info->set_phone_verified((user_info_iter + 6)->GetBool());
+    embeded_user_info->set_phone_verified(user_info[6].GetBool());
 
     // danmaku
     // 设置字符串的函数会分配额外的string 并且不通过arena分配内存
     // 需要考虑tcmalloc等
-    ASSERT_TRACE((info_iter + 1)->IsString())
+    ASSERT_TRACE(info[1].IsString())
     // message
-    danmaku->set_message((info_iter + 1)->GetString(), (info_iter + 1)->GetStringLength());
-    ASSERT_TRACE((basic_info_iter + 9)->IsUint())
+    embeded_danmaku->set_message(info[1].GetString(), info[1].GetStringLength());
+    ASSERT_TRACE(basic_info[9].IsUint())
     // danmaku type
-    switch ((basic_info_iter + 9)->GetUint())
+    switch (basic_info[9].GetUint())
     {
     case 0:  // 普通弹幕
-        danmaku->set_lottery_type(live::LotteryDanmakuType::NO_LOTTERY);
+        embeded_danmaku->set_lottery_type(live::LotteryDanmakuType::NO_LOTTERY);
         break;
     case 1:  // 节奏风暴
-        danmaku->set_lottery_type(live::LotteryDanmakuType::STORM);
+        embeded_danmaku->set_lottery_type(live::LotteryDanmakuType::STORM);
         break;
     case 2:  // 抽奖弹幕
-        danmaku->set_lottery_type(live::LotteryDanmakuType::LOTTERY);
+        embeded_danmaku->set_lottery_type(live::LotteryDanmakuType::LOTTERY);
         break;
     default:
         // TODO: 错误处理
         break;
     }
-    ASSERT_TRACE((info_iter + 7)->IsUint())
+    ASSERT_TRACE(info[7].IsUint())
     // guard level
-    switch ((info_iter + 7)->GetUint())
+    switch (info[7].GetUint())
     {
     case 0:  // 无舰队
-        danmaku->set_guard_level(live::GuardLevel::NO_GUARD);
+        embeded_danmaku->set_guard_level(live::GuardLevel::NO_GUARD);
         break;
     case 1:  // 总督
-        danmaku->set_guard_level(live::GuardLevel::LEVEL3);
+        embeded_danmaku->set_guard_level(live::GuardLevel::LEVEL3);
         break;
     case 2:  // 提督
-        danmaku->set_guard_level(live::GuardLevel::LEVEL2);
+        embeded_danmaku->set_guard_level(live::GuardLevel::LEVEL2);
         break;
     case 3:  // 舰长
-        danmaku->set_guard_level(live::GuardLevel::LEVEL1);
+        embeded_danmaku->set_guard_level(live::GuardLevel::LEVEL1);
     default:
         // TODO: 错误处理
         break;
     }
 
-    user_message->set_allocated_user(user_info);
-    user_message->set_allocated_danmaku(danmaku);
-    message->set_allocated_user_message(user_message);
+    embeded_user_message->set_allocated_user(embeded_user_info);
+    embeded_user_message->set_allocated_danmaku(embeded_danmaku);
+    message->set_allocated_user_message(embeded_user_message);
+    return true;
+}
+
+CMD(SUPER_CHAT_MESSAGE)
+{
+    // TODO: 更好的命名
+    auto const& _data = document["data"];
+    auto const& _user_info = _data["user_info"];
+    return true;
 }
 
 class borrowed_message : public borrowed_buffer
@@ -225,23 +220,23 @@ public:
     /// @return json转换为的protobuf序列化后的buffer。
     const borrowed_message* serialize(char* buf, const unsigned int& room_id)
     {
+        _message->Clear();
         _document.ParseInsitu(buf);
         _message->set_room_id(room_id);
         // TODO: 使用boost::multiindex配合robin_hood::hash魔改robin_hood::unordered_map来避免无意义的内存分配
+        if (!(_document.HasMember("cmd")
+              && _document["cmd"].IsString()))
+        {
+            SPDLOG_TRACE("[bili_json] bilibili json cmd type check failed");
+            return nullptr;
+        }
         string cmd(_document["cmd"].GetString(), _document["cmd"].GetStringLength());
-        // TODO: 简化太过分的检查
-        // TODO: 针对不同的错误处理方式分拆if
-        if (_document.HasMember("cmd")
-            && _document["cmd"].IsString()
-            && command.find(cmd) != command.end())
-        {
-            command[cmd](_document, _message, &_arena);
-        }
-        else
-        {
-            // TODO: 错误处理
-        }
-        return &_borrowed_message;
+        if ((command.find(cmd) != command.end())
+            && command[cmd](_document, _message, &_arena))
+            return &_borrowed_message;
+        // 下面的代码会拼接字符串 但当编译选项为release时 日志宏不会启用
+        SPDLOG_TRACE("[bili_json] bilibili json unknown cmd field: " + cmd);
+        return nullptr;
     }
     ~parse_context();
 };
