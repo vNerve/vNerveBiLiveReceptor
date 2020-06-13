ff2a50cfe12328fd2b92457eb40cd22ed2668ac4
Charlie Jiang <cqjjjzr@126.com>
2020-06-13 13:35:28 +0800

    :bug: (worker): fix borrowed_message

--- src/worker/borrowed_message.h
+++ src/worker/borrowed_message.h
@@ -1,14 +1,17 @@
 #pragma once
 
+#include "simple_worker_proto.h"
+
 namespace vNerve::bilibili
 {
 class borrowed_message
 {
 public:
     int crc32;
-    // TODO: use size constant from shared folder
-    char routing_key[24];
+    char routing_key[worker_supervisor::routing_key_max_size];
     virtual size_t size() = 0;
     virtual void write(void* data, int size) = 0;
+
+    virtual ~borrowed_message() = default;
 };
 }  // namespace vNerve
