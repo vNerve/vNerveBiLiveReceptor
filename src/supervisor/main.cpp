90a95ba67b784609521951e5eb7ce0f0cdb6e914
Charlie Jiang <cqjjjzr@126.com>
2020-06-25 21:19:34 +0800

    :bug: (global): Fix logging level not reading from env

--- src/supervisor/main.cpp
+++ src/supervisor/main.cpp
@@ -7,9 +7,9 @@
 
 int main(int argc, char** argv)
 {
+    spdlog::set_level(spdlog::level::info);
     spdlog::cfg::load_env_levels();
     auto opt = vNerve::bilibili::config::parse_options(argc, argv);
-    spdlog::set_level(spdlog::level::info);
     auto gc = new vNerve::bilibili::supervisor_global_context(opt);
     gc->join();
     delete gc;
--- src/worker/main.cpp
+++ src/worker/main.cpp
@@ -11,8 +11,8 @@ int main(int argc, char** argv)
 {
     vNerve::bilibili::util::enable_windows_minidump();
     // TODO main.
-    spdlog::cfg::load_env_levels();
     spdlog::set_level(spdlog::level::info);
+    spdlog::cfg::load_env_levels();
     global_config = vNerve::bilibili::config::parse_options(argc, argv);
     auto global_ctxt = new vNerve::bilibili::worker_global_context(global_config);
     global_ctxt->join();
