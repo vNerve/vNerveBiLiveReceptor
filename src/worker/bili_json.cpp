7ddd718a564d27069e46d88207784df4a600886f
Charlie Jiang <cqjjjzr@126.com>
2020-07-07 23:34:04 +0800

    :construction: (worker): WIP bili_json.

--- src/worker/bili_json.cpp
+++ src/worker/bili_json.cpp
@@ -191,6 +191,26 @@ const borrowed_message* serialize_popularity(const long long popularity, const u
     auto const& name = name##_iter->value;                               \
     ASSERT_TRACE(expr)
 
+void set_guard_level(unsigned int level, live::UserInfo* user_info)
+{
+    switch (level)
+    {
+    case 0:  // 无舰队
+        user_info->set_guard_level(live::GuardLevel::NO_GUARD);
+        break;
+    case 1:  // 总督
+        user_info->set_guard_level(live::GuardLevel::LEVEL3);
+        break;
+    case 2:  // 提督
+        user_info->set_guard_level(live::GuardLevel::LEVEL2);
+        break;
+    case 3:  // 舰长
+        user_info->set_guard_level(live::GuardLevel::LEVEL1);
+    default:
+        SPDLOG_TRACE("[bili_json] unknown guard level");
+    }
+}
+
 CMD(DANMU_MSG)
 {
     // 尽管所有UserMessage都需要设置UserInfo
@@ -250,9 +270,9 @@ CMD(DANMU_MSG)
     {
         if (user_info[3].GetInt() == 1)
             // 月费会员为真 年费会员为假 月费
-            embedded_user_info->set_live_vip_level(live::LiveVipLevel::MONTHLY);
+            embedded_user_info->set_live_vip_level(live::LiveVipLevel::MONTHLY_VIP);
         else  // 月费会员为假 年费会员为真 年费
-            embedded_user_info->set_live_vip_level(live::LiveVipLevel::YEARLY);
+            embedded_user_info->set_live_vip_level(live::LiveVipLevel::YEARLY_VIP);
     }
     // regular user
     ASSERT_TRACE(user_info[5].IsNumber())
@@ -340,78 +360,39 @@ CMD(DANMU_MSG)
     // guard level
     auto const& guard_level = info[7];
     ASSERT_TRACE(guard_level.IsUint())
-    switch (guard_level.GetUint())
-    {
-    case 0:  // 无舰队
-        embedded_danmaku->set_guard_level(live::GuardLevel::NO_GUARD);
-        break;
-    case 1:  // 总督
-        embedded_danmaku->set_guard_level(live::GuardLevel::LEVEL3);
-        break;
-    case 2:  // 提督
-        embedded_danmaku->set_guard_level(live::GuardLevel::LEVEL2);
-        break;
-    case 3:  // 舰长
-        embedded_danmaku->set_guard_level(live::GuardLevel::LEVEL1);
-    default:
-        SPDLOG_TRACE("[bili_json] unknown guard level");
-    }
+    set_guard_level(guard_level.GetInt(), embedded_user_info);
 
-    //if (medal_info_present)
-        //embedded_user_info->clear_medal();
-    //embedded_user_message->set_allocated_user(embedded_user_info);
-    //embedded_user_message->set_allocated_danmaku(embedded_danmaku);
-    //message._message->set_allocated_user_message(embedded_user_message);
     return true;
 }
 
 CMD(SUPER_CHAT_MESSAGE)
 {
     ROUTING_KEY("blv.{}.sc")
-
-    // TODO: 补充SC中的字段
-
-    // 以下变量均为 rapidjson::GenericArray ?
-    // TODO: 全部改成上面写的 GetMemberCheck
-    // TODO: 检查如果没有带牌子，medal_info状态
-    ASSERT_TRACE(document.HasMember("data"))
-    ASSERT_TRACE(document["data"].IsObject())
-    auto const& data = document["data"];
-    ASSERT_TRACE(data.HasMember("user_info"))
-    ASSERT_TRACE(data["user_info"].IsObject())
-    auto const& user_info = data["user_info"];
-    ASSERT_TRACE(data.HasMember("medal_info"))
-    ASSERT_TRACE(data["medal_info"].IsObject())
-    auto const& medal_info = data["medal_info"];
-    // 以下变量类型均为 T*
+    GetMemberCheck(document, data, document["data"].IsObject())
+    GetMemberCheck(data, user_info, data["user_info"].IsObject())
     auto embedded_user_message = message._message->mutable_user_message();
     auto embedded_user_info = embedded_user_message->mutable_user();
-    auto embedded_medal_info = embedded_user_info->mutable_medal();
-    auto embedded_superchat = Arena::CreateMessage<live::SuperChatMessage>(arena);
+    auto embedded_superchat = embedded_user_message->mutable_super_chat();
 
     // user_info
     // uid
-    ASSERT_TRACE(data.HasMember("uid"))
-    ASSERT_TRACE(data["uid"].IsUint64())
-    embedded_user_info->set_uid(data["uid"].GetUint64());
+    GetMemberCheck(data, uid, uid.IsUint64())
+    embedded_user_info->set_uid(uid.GetUint64());
     // uname
-    ASSERT_TRACE(user_info.HasMember("uname"));
-    ASSERT_TRACE(user_info["uname"].IsString())
-    embedded_user_info->set_name(user_info["uname"].GetString(), user_info["uname"].GetStringLength());
+    GetMemberCheck(user_info, uname, uname.IsString())
+    embedded_user_info->set_name(uname.GetString(), uname.GetStringLength());
     // admin
-    ASSERT_TRACE(user_info.HasMember("manager"));
-    ASSERT_TRACE(user_info["manager"].IsBool())
-    embedded_user_info->set_admin(user_info["manager"].GetBool());
+    GetMemberCheck(user_info, manager, manager.IsBool())
+    embedded_user_info->set_admin(user_info.GetBool());
+
     // vip&svip->livevip
-    ASSERT_TRACE(user_info.HasMember("is_vip")
-                 & user_info.HasMember("is_svip")
-                 & user_info["is_vip"].IsBool()
-                 & user_info["is_svip"].IsBool())
+    GetMemberCheck(user_info, is_vip, is_vip.IsInt())
+    GetMemberCheck(user_info, is_svip, is_svip.IsInt())
     // 这两个字段分别是月费/年费会员
     // 都为假时无会员 都为真时报错
-    if (user_info["is_vip"].GetBool() == user_info["is_svip"].GetBool())
+    if (is_vip.GetInt() == is_svip.GetInt())
     {
-        if (user_info["is_vip"].GetBool())
+        if (is_vip.GetInt())
             // 均为真 报错
             // 未设置的protobuf字段会被置为默认值
             SPDLOG_TRACE("[bili_json] both vip and svip are true");
@@ -420,11 +401,11 @@ CMD(SUPER_CHAT_MESSAGE)
     }
     else
     {
-        if (user_info["is_vip"].GetBool())
+        if (is_vip.GetInt())
             // 月费会员为真 年费会员为假 月费
-            embedded_user_info->set_live_vip_level(live::LiveVipLevel::MONTHLY);
+            embedded_user_info->set_live_vip_level(live::LiveVipLevel::MONTHLY_VIP);
         else  // 月费会员为假 年费会员为真 年费
-            embedded_user_info->set_live_vip_level(live::LiveVipLevel::YEARLY);
+            embedded_user_info->set_live_vip_level(live::LiveVipLevel::YEARLY_VIP);
     }
     // regular user
     // phone_verified
@@ -432,93 +413,102 @@ CMD(SUPER_CHAT_MESSAGE)
     // 但是赠送礼物的理应可以视为正常用户 而不是默认值的非正常用户
     embedded_user_info->set_regular_user(true);
     embedded_user_info->set_phone_verified(true);
-    ASSERT_TRACE(user_info[6].IsBool())
     // user_level
-    ASSERT_TRACE(user_info.HasMember("user_level"))
-    ASSERT_TRACE(user_info["user_level"].IsUint())
-    embedded_user_info->set_user_level(user_info["user_level"].GetUint());
+    GetMemberCheck(user_info, user_level, user_level.IsUint())
+    embedded_user_info->set_user_level(user_level.GetUint());
     // user_level_border_color
-    // SC似乎没有这个字段
+    GetMemberCheck(user_info, level_color, level_color.IsString())
+    if (level_color.GetStringLength() == 7)
+    {
+        auto level_color_ul = std::strtoul(level_color.GetString() + 1, nullptr, 16);
+        embedded_user_info->set_user_level_border_color(level_color_ul);
+    }
+
     // title
     // 值得注意的是 弹幕和礼物的title默认值是空字符串""
     // 但SC的title默认值是"0"
     // 需要考虑是否进行处理
-    ASSERT_TRACE(user_info.HasMember("title"))
-    ASSERT_TRACE(user_info["title"].IsString())
-    embedded_user_info->set_title(user_info["title"].GetString(), user_info["title"].GetStringLength());
-    ASSERT_TRACE(user_info.HasMember("is_main_vip"))
+    GetMemberCheck(user_info, title, title.IsString())
+    if (std::strncmp(title.GetString(), "0", 1) == 0)
+        embedded_user_info->set_title("");
+    else
+        embedded_user_info->set_title(title.GetString(), title.GetStringLength());
+
+    GetMemberCheck(user_info, is_main_vip, is_main_vip.IsInt())
+    embedded_user_info->set_main_vip(is_main_vip == 1);
+
     // avatar_url
-    ASSERT_TRACE(user_info.HasMember("face"))
-    ASSERT_TRACE(user_info["face"].IsString())
-    embedded_user_info->set_avatar_url(user_info["face"].GetString(), user_info["face"].GetStringLength());
-    // user_info["face_frame"] 舰长框
+    GetMemberCheck(user_info, face, face.IsString())
+    embedded_user_info->set_avatar_url(face.GetString(), face.GetStringLength());
+    // user_info["face_frame"]
     // 没有对应的字段
-    // main_vip
-    ASSERT_TRACE(user_info["is_main_vip"].IsBool())
-    embedded_user_info->set_main_vip(user_info["is_main_vip"].GetBool());
+    // guard_level
+    GetMemberCheck(user_info, guard_level, guard_level.IsUint())
+    set_guard_level(guard_level.GetUint(), embedded_user_info);
 
     // medal
-    // medal_name
-    ASSERT_TRACE(medal_info.HasMember("medal_name"))
-    ASSERT_TRACE(medal_info["medal_name"].IsString())
-    embedded_medal_info->set_medal_name(medal_info["medal_name"].GetString(), medal_info["medal_name"].GetStringLength());
-    // medal_level
-    ASSERT_TRACE(medal_info.HasMember("medal_level"))
-    ASSERT_TRACE(medal_info["medal_level"].IsUint())
-    embedded_medal_info->set_medal_level(medal_info["medal_level"].GetUint());
-    // medal_color
-    ASSERT_TRACE(medal_info.HasMember("medal_color"))
-    ASSERT_TRACE(medal_info["medal_color"].IsUint())
-    embedded_medal_info->set_medal_color(medal_info["medal_color"].GetUint());
-    // liver user id
-    ASSERT_TRACE(medal_info.HasMember("target_id"))
-    ASSERT_TRACE(medal_info["target_id"].IsUint())  // IsUint->Uint64存疑
-    embedded_medal_info->set_streamer_uid(medal_info["target_id"].GetUint64());
-    // liver user name
-    ASSERT_TRACE(medal_info.HasMember("anchor_uname"))
-    ASSERT_TRACE(medal_info["anchor_uname"].IsString())
-    embedded_medal_info->set_streamer_name(medal_info["anchor_uname"].GetString(), medal_info["anchor_uname"].GetStringLength());
-    // liver room id
-    ASSERT_TRACE(medal_info.HasMember("anchor_roomid"))
-    ASSERT_TRACE(medal_info["anchor_roomid"].IsUint())
-    embedded_medal_info->set_streamer_roomid(medal_info["anchor_roomid"].GetUint());
-    // special_medal
-    // medal_info[6] unknown
-    // medal_info[7] unknown
+    auto medal_info_iter = data.FindMember("medal_info");
+    if (medal_info_iter != data.MemberEnd()
+        && medal_info_iter->value.IsObject()
+        && medal_info_iter->value.HasMember("target_id"))
+    {
+        auto const& medal_info = medal_info_iter->value;
+        auto embedded_medal_info = embedded_user_info->mutable_medal();
+
+        // medal_name
+        GetMemberCheck(medal_info, medal_name, medal_name.IsString())
+        embedded_medal_info->set_medal_name(medal_name.GetString(), medal_name.GetStringLength());
+        // medal_level
+        GetMemberCheck(medal_info, medal_level, medal_level.IsUint())
+        embedded_medal_info->set_medal_level(medal_level.GetUint());
+        // medal_color
+        GetMemberCheck(medal_info, medal_color, medal_color.IsString())
+        if (medal_color.GetStringLength() == 7)
+        {
+            auto medal_color_ul = std::strtoul(medal_color.GetString(), nullptr, 16);
+            embedded_medal_info->set_medal_color(medal_color_ul);
+        }
+
+        // liver user id
+        GetMemberCheck(medal_info, target_id, target_id.IsUint())
+        embedded_medal_info->set_streamer_uid(target_id.GetUint64());
+        // liver user name
+        GetMemberCheck(medal_info, anchor_uname, anchor_uname.IsString())
+        embedded_medal_info->set_streamer_name(anchor_uname.GetString(), anchor_uname.GetStringLength());
+        // liver room id
+        GetMemberCheck(medal_info, anchor_roomid, anchor_roomid.IsUint())
+        embedded_medal_info->set_streamer_roomid(anchor_roomid.GetUint());
+    }
+    else
+        embedded_user_info->clear_medal();
 
     // superchat
     // id
-    ASSERT_TRACE(data.HasMember("id"))
-    ASSERT_TRACE(data["id"].IsUint());
-    embedded_superchat->set_id(data["id"].GetUint());
+    GetMemberCheck(data, id, id.IsUint())
+    embedded_superchat->set_id(id.GetUint());
     // message
-    ASSERT_TRACE(data.HasMember("message"))
-    ASSERT_TRACE(data["message"].IsString())
-    embedded_superchat->set_message(data["message"].GetString(), data["message"].GetStringLength());
+    auto content_iter = data.FindMember("message");
+    ASSERT_TRACE(content_iter != data.MemberEnd())
+    auto const& content = content_iter->value;
+    embedded_superchat->set_message(content.GetString(), content.GetStringLength());
     // price
-    ASSERT_TRACE(data.HasMember("price"))
-    ASSERT_TRACE(data["price"].IsUint())
-    embedded_superchat->set_price(data["price"].GetUint());
+    GetMemberCheck(data, price, price.IsInt())
+    embedded_superchat->set_price_cny(price.GetUint());
+    GetMemberCheck(data, rate, rate.IsUint())
+    embedded_superchat->set_price_coin(price.GetUint() * rate.GetUint());
     // token
-    ASSERT_TRACE(data.HasMember("token"))
-    ASSERT_TRACE(data["token"].IsString())
-    embedded_superchat->set_token(data["token"].GetString(), data["token"].GetStringLength());
+    GetMemberCheck(data, token, token.IsString())
+    embedded_superchat->set_token(token.GetString(), token.GetStringLength());
     // lasting_time_sec
-    ASSERT_TRACE(data.HasMember("time"))
-    ASSERT_TRACE(data["time"].IsUint())
-    embedded_superchat->set_lasting_time_sec(data["time"].GetUint());
+    GetMemberCheck(data, time, time.IsUint())
+    embedded_superchat->set_lasting_time_sec(time.GetUint());
     // start_time
-    ASSERT_TRACE(data.HasMember("start_time"))
-    ASSERT_TRACE(data["start_time"].IsUint64())
-    embedded_superchat->set_start_time(data["start_time"].GetUint64());
+    GetMemberCheck(data, start_time, start_time.IsUint64())
+    embedded_superchat->set_start_time(start_time.GetUint64());
     // end_time
-    ASSERT_TRACE(data.HasMember("end_time"))
-    ASSERT_TRACE(data["end_time"].IsUint64())
-    embedded_superchat->set_end_time(data["end_time"].GetUint64());
+    GetMemberCheck(data, end_time, start_time.IsUint64())
+    embedded_superchat->set_start_time(end_time.GetUint64());
 
-    embedded_user_info->set_allocated_medal(embedded_medal_info);
-    embedded_user_message->set_allocated_user(embedded_user_info);
-    embedded_user_message->set_allocated_super_chat(embedded_superchat);
     message._message->set_allocated_user_message(embedded_user_message);
 
     return true;
@@ -529,23 +519,40 @@ CMD(SEND_GIFT)
     ROUTING_KEY("blv.{}.gift")
 
     // 以下变量均为 rapidjson::GenericArray ?
-    ASSERT_TRACE(document.HasMember("data"))
-    ASSERT_TRACE(document["data"].IsObject())
-    auto const& data = document["data"];
+    GetMemberCheck(document, data, data.IsObject())
     // 以下变量类型均为 T*
-    auto embedded_user_message = Arena::CreateMessage<live::UserMessage>(arena);
-    auto embedded_user_info = Arena::CreateMessage<live::UserInfo>(arena);
-    auto embedded_gift = Arena::CreateMessage<live::GiftMessage>(arena);
-    // 礼物似乎没有牌子信息
-    // auto embedded_medal_info = Arena::CreateMessage<live::MedalInfo>(arena);
+    auto embedded_user_message = message._message->mutable_user_message();
+    auto embedded_user_info = embedded_user_message->mutable_user();
+    auto embedded_gift = embedded_user_message->mutable_gift();
 
-    // TODO
+    // user_info
+    GetMemberCheck(data, uid, uid.IsUint64())
+    embedded_user_info->set_uid(uid.GetUint64());
+    GetMemberCheck(data, uname, uname.IsString())
+    embedded_user_info->set_name(uname.GetString(), uname.GetStringLength());
+    GetMemberCheck(data, face, face.IsString())
+    embedded_user_info->set_avatar_url(face.GetString(), face.GetStringLength());
 
-    // 礼物似乎没有牌子信息
-    // embedded_user_info->set_allocated_medal(embedded_medal_info);
-    embedded_user_message->set_allocated_user(embedded_user_info);
-    embedded_user_message->set_allocated_gift(embedded_gift);
-    message._message->set_allocated_user_message(embedded_user_message);
+    // gift
+    GetMemberCheck(data, coin_type, coin_type.IsString())
+    if (std::strncmp(
+        "gold", coin_type.GetString(),
+        std::min(4, static_cast<int>(coin_type.GetStringLength()))))
+        embedded_gift->set_is_gold(true);
+    else
+        embedded_gift->set_is_gold(false);
+    GetMemberCheck(data, total_coin, total_coin.IsUint())
+    embedded_gift->set_total_coin(total_coin.GetUint());
+    GetMemberCheck(data, price, price.IsUint())
+    embedded_gift->set_single_price_coin_raw(price.GetUint());
+
+    GetMemberCheck(data, giftId, giftId.IsUint())
+    embedded_gift->set_gift_id(giftId.GetUint());
+    GetMemberCheck(data, giftName, giftName.IsString())
+    embedded_gift->set_gift_name(giftName.GetString(), giftName.GetStringLength());
+    GetMemberCheck(data, num, num.IsUint());
+    embedded_gift->set_count(num.GetUint());
+
     return true;
 }
 
